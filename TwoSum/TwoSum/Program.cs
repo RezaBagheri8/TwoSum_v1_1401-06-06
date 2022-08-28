afcfab2492af767565feff186831472441a40431
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-28 15:35:43 +0430

    Initial Repo

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using TwoSum;
+
+int[] array = { 2,7,11,15};
+
+TwoSumImplement twoSum = new TwoSumImplement();
+
+int[] result = twoSum.TwoSum(array, 9);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
---
+++ TwoSum/TwoSum/TwoSumImplement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class TwoSumImplement
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int firstIndex = 0;
+            int secondIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        firstIndex = i;
+                        secondIndex = j;
+                        break;
+                    }
+                }
+            }
+
+            int[] result = new int[2];
+            result[0] = firstIndex;
+            result[1] = secondIndex;
+            return result;
+        }
+    }
+}
